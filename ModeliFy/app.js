88d85dced5455e9c5f711025ff8b972f7c5f257d
jose ramos <ramoschoquej.l.389@gmail.com>
2025-01-09 10:36:34 -0300

    Requisitos del sprint3

--- ModeliFy/app.js
+++ ModeliFy/app.js
@@ -4,28 +4,34 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-console.log(path.join(__dirname,'public'));
+// console.log(path.join(__dirname,'public'));
+
+app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname,'public')));
-console.log(path.join(__dirname,'views', 'home.html'));
+// console.log(path.join(__dirname,'views', 'home.html'));
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views', 'home.html')); 
+    res.render(path.join(__dirname,'views', 'home.ejs')); 
 });
 
 app.get('/login',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views', 'login.html'));
+    res.render(path.join(__dirname,'views', 'login.ejs'));
 });
 
 app.get('/product',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views', 'productNotFree.html'));
+    res.render(path.join(__dirname,'views', 'product.ejs'));
 });
 
 app.get('/cart',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views', 'cart.html'));
+    res.render(path.join(__dirname,'views', 'cart.ejs'));
+});
+
+app.get('/new-product',(req,res)=>{
+    res.render(path.join(__dirname,'views', 'addProduct.ejs'));
 });
 
 
 
 
-app.listen(port, ()=>console.log("Servidor corriendo en el puerto: https://localhost:" + port))
+app.listen(port, ()=>console.log("Servidor corriendo en el puerto: http://localhost:" + port))
